aa9eddad437f6ab74d153e0546b2655cb4def44a
Ziuwar <Ziuwar@gmail.com>
2017-12-26 01:11:44 +0100

    a few tests

--- EveLpApp/wxPythonEval.py
+++ EveLpApp/wxPythonEval.py
@@ -3,13 +3,14 @@
 import wx
 import wx.grid as gridlib
 
+
 class EveLpApp(wx.Frame):
 
     def __init__(self, *args, **kwargs):
         super(EveLpApp, self).__init__(*args, **kwargs)
 
         self.InitUi()
-        self.FillTable()
+        self.fill_table()
         self.SetSize((1100, 580))
         self.SetTitle('EveLpApp')
         self.Centre()
@@ -17,16 +18,16 @@ class EveLpApp(wx.Frame):
 
     def InitUi(self):
 
-        #Make me a little grid you codeing bitch, thanks.
+        # Make me a little grid you codeing bitch, thanks.
         LpPanel = wx.Panel(self, wx.ID_ANY)
         self.grid = gridlib.Grid(LpPanel)
         self.grid.CreateGrid(25,9)
         self.grid.EnableEditing(False)
-        #Box layout
+        # Box layout
         Sizer = wx.BoxSizer(wx.VERTICAL)
         Sizer.Add(self.grid, 1, wx.EXPAND, 5)
         LpPanel.SetSizer(Sizer)
-        #Column config
+        # Column config
         self.grid.SetColMinimalAcceptableWidth(110)
         self.grid.SetColLabelValue(0, 'Item Name')
         self.grid.SetColSize(0,110)
@@ -55,51 +56,51 @@ class EveLpApp(wx.Frame):
         self.grid.SetColFormatFloat(8, -1 , 2)
         self.grid.SetColSize(8,110)
 
-        #Create a menu bar
+        # Create a menu bar
         MenuBar = wx.MenuBar()
 
-        #Create the file menu in the menu bar
+        # Create the file menu in the menu bar
         FileMenu = wx.Menu()
         FileMenu.Append(wx.ID_NEW, '&New')
         FileMenu.Append(wx.ID_DELETE, '&Delete')
         FileMenu.Append(wx.ID_ANY, '&Update\tAlt+F5')
         FileMenu.AppendSeparator()
         Qmi = wx.MenuItem(FileMenu, wx.ID_EXIT, '&Quit\tAlt+F4')
-        #Qmi.SetBitmap(wx.Bitmap('exit.png'))
+        # Qmi.SetBitmap(wx.Bitmap('exit.png'))
         FileMenu.Append(Qmi)
 
-        #Create the help menu in the bar
+        # Create the help menu in the bar
         HelpMenu = wx.Menu()
         HelpMenu.Append(wx.ID_ABOUT, '&About\tF1')
 
-        #Bound functions in the menu bar
-        self.Bind(wx.EVT_MENU, self.OnQuit, Qmi)
+        # Bound functions in the menu bar
+        self.Bind(wx.EVT_MENU, self.on_quit, Qmi)
 
-        #Append the menus to the bar
+        # Append the menus to the bar
         MenuBar.Append(FileMenu, '&File')
         MenuBar.Append(HelpMenu, '&Help')
         self.SetMenuBar(MenuBar)
 
-    #Method for filling the table with data
-    def FillTable(self):
+    # Method for filling the table with data
+    def fill_table(self):
         GridFill = ('Astero', 15000000.00, 30000, 387669.41, 15387669.41, 62300000.00, 46912330.59, 404.87 , 1563.74)
         for i in range(0,len(GridFill),1):
-            Value = str(GridFill[i])
-            self.grid.SetCellValue(0,i,Value)
+            value = str(GridFill[i])
+            self.grid.SetCellValue(0, i, value)
 
-    #Method to close the instance
-    def OnQuit(self, e):
+    # Method to close the instance
+    def on_quit(self, e):
         self.Close()
 
-def Main():
+
+def main():
     app = wx.App()
     x = EveLpApp(None, title='EveLpApp')
-    #x.grid.SetCellValue(1,0,'FUCK')
+    # x.grid.SetCellValue(1,0,'FUCK')
     app.MainLoop() 
 
-    #print(x.grid)
+    # print(x.grid)
+
 
 if __name__ == '__main__':
-    Main()
-
-    
+    main()
